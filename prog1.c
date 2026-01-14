8ae858ebfd981e99804f8f852c15ae4d90ffcb2b
Benjamin Bernosky <bernoskb@flip2.engr.oregonstate.edu>
2026-01-13 16:34:57 -0800

    Complete program

--- prog1.c
+++ prog1.c
@@ -1,11 +1,35 @@
-#include <stdio.h> // Necessary for printf()
+#include <stdio.h> // Necessary for print
+#include <stdlib.h> // Adds standard library of functions including the random
+//myrandgenerator
 
-// TODO Other includes
+
 
 
 int main() {
-	// The main function is the entrypoint of the program. When the program
-	// is executed, it will automatically start here.
+	srand(time(NULL)); // Seeds the random myrandgenerator
 	
-	// TODO Complete the program
+	int myrand = rand() % 11; //random integer from 0-10
+	
+	printf("The number chosen is: %d\n",myrand); //prints the random integer
+
+	if (myrand <= 4) { //prints statement when int is between 0 and 4
+
+		 printf("Eat more beef\n"); 
+	}
+	else if (myrand> 4 && myrand < 10) { //prints statement when int is between
+										// 5 and 9
+
+		printf("FRODO LIVES\n");
+	}
+
+	else {	// prints statement when int equals 10
+		
+		printf("Larn is the best rougelike\n");
+	}
+
+
+
+
+
+
 }
